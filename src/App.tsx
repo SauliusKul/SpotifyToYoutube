c34ed8ef5ebe89995ce17d9f302c0c3fac91e77c
Saulius Kulakauskas <kulakauskas.saulius@gmail.com>
2022-12-22 11:53:23 +0200

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter } from 'react-router-dom';
+
+
+function App(): JSX.Element {
+
+  return (
+    <BrowserRouter>
+      {/* <NavBar>
+
+      </NavBar> */}
+      <div>
+        Hello world!
+      </div>
+    
+    </ BrowserRouter>
+  )
+}
+
+export default App
